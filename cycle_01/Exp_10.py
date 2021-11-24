36d87adfecc597300e656aa3d76f931c4c38aa0f
Aksa Babu <93571054+AksaBabu@users.noreply.github.com>
2021-11-24 11:44:43 +0530

    Update Exp_10.py

--- cycle_01/Exp_10.py
+++ cycle_01/Exp_10.py
@@ -1,8 +1,5 @@
+import math
 num1=int(input("Enter the first number"))
 num2=int(input("Enter the second number"))
-i=1
-while i<=num1 and i<=num2:
-  if(num1%i==0 and num2%i==0):
-      gcd=i
-      i=i+1
-print("GCD is",gcd) 
+
+print("GCD is",math.gcd(num1,num2)) 
